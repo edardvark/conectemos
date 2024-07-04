723e50e666526d0adaa237eda6a740c7ce7a2bd3
edardvark <94202593+edardvark@users.noreply.github.com>
2024-07-04 14:33:52 -0500

    Update cards_UI.py

--- cards_UI.py
+++ cards_UI.py
@@ -5,7 +5,7 @@ import base64
 df = pd.read_csv('conectemos_preguntas.csv')
 pregunta = df['Preguntas'].sample(1).values[0]
 
-filepath = "C:\\Users\\sabdi\\Documents\\Conectemos\\conectm.png"
+filepath = "https://github.com/edardvark/conectemos/blob/main/conectm.png"
 with open(filepath, "rb") as f:
     data = f.read()
     encoded = base64.b64encode(data)
