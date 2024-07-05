f2d66e387f1bc0b82bf9272fa0b57b782a1dc12d
edardvark <94202593+edardvark@users.noreply.github.com>
2024-07-05 15:06:23 -0500

    Update cards_UI.py

--- cards_UI.py
+++ cards_UI.py
@@ -17,7 +17,7 @@ hasClicked = card(
   image=data,
   styles={
         "card": {
-            "width": "300px",
+            "width": "50%",
             "height": "450px",
             "border-radius": "30px",
             "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
