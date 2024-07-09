006ea67691a3de59d460f984a950b63d98c37b3c
edardvark <94202593+edardvark@users.noreply.github.com>
2024-07-09 14:44:19 -0500

    Update cards_UI.py

--- cards_UI.py
+++ cards_UI.py
@@ -115,3 +115,11 @@ else:
     }}
     </style>
     """, unsafe_allow_html=True)
+    
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
